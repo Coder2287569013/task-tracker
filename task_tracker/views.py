1ae8b0736507c91c0d0d6d8237631445d229c915
Coder2287569013 <yarovenkovova2008@gmail.com>
2024-10-06 10:12:20 -0230

    Third

--- task_tracker/views.py
+++ task_tracker/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, CreateView, DetailView
+from django.urls import reverse_lazy
+from .forms import TaskForm
+from .models import Task
 # Create your views here.
+
+class TaskListView(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+
+class TaskDetailView(DetailView):
+    model = Task
+    context_object_name = 'task'
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy('task-list')
